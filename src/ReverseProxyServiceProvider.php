f777760efedbdc17786b53ece9e66e08f3ebc72c
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2019-08-07 12:51:32 +0000

    Added service provider

---
+++ src/ReverseProxyServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProbablyRational\ReverseProxy;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReverseProxyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
